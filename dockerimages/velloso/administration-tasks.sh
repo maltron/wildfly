e1dbfc9ca6ebeb6d75ed5651c3d379b5b362964d
root <root@openshift.example.com>
2017-01-05 20:05:21 -0500

    Modified to alpineos

--- dockerimages/velloso/administration-tasks.sh
+++ dockerimages/velloso/administration-tasks.sh
@@ -2,15 +2,15 @@
 echo ">>> Step 1: Switching to Project/Namespace OpenShift"
 oc project openshift
 # Delete previous image 
-echo ">>> Step 2: Deleting previous existing ImageStream: tomcat7alpine and oracle-xe-11g"
-oc delete imagestream/tomcat7alpine --namespace openshift
+echo ">>> Step 2: Deleting previous existing ImageStream: tomcat7alpineos and oracle-xe-11g"
+oc delete imagestream/tomcat7alpineos --namespace openshift
 oc delete imagestream/oracle-xe-11g --namespace openshift
 oc delete template/tomcat-velloso --namespace openshift
 # Creating template into Project/Namespace Openshift
 oc create -f template/tomcat-velloso.yaml --namespace openshift
 # Import the image
 echo ">>> Importing Image: docker.io/fabiovelloso/tomcat7alpine:latest"
-oc import-image --confirm=true --insecure=true docker.io/fabiovelloso/tomcat7alpine:latest --namespace openshift
+oc import-image --confirm=true --insecure=true docker.io/fabiovelloso/tomcat7alpineos:latest --namespace openshift
 echo ">>> Importing Image: docker.io/wnameless/oracle-xe-11g:latest"
 oc import-image --confirm=true --insecure=true docker.io/wnameless/oracle-xe-11g:latest --namespace openshift
 # Let user: demo into scc/anyuid
