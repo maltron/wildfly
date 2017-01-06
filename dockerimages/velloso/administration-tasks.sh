389ab3f29ce700e08bb8fb5ab2338cd3391421b9
root <root@openshift.example.com>
2017-01-05 19:36:01 -0500

    Added more information into administration script

--- dockerimages/velloso/administration-tasks.sh
+++ dockerimages/velloso/administration-tasks.sh
@@ -5,8 +5,17 @@ oc project openshift
 echo ">>> Step 2: Deleting previous existing ImageStream: tomcat7alpine and oracle-xe-11g"
 oc delete imagestream/tomcat7alpine --namespace openshift
 oc delete imagestream/oracle-xe-11g --namespace openshift
+oc delete template/tomcat-velloso --namespace openshift
+# Creating template into Project/Namespace Openshift
+oc create -f template/tomcat-velloso.yaml --namespace openshift
 # Import the image
 echo ">>> Importing Image: docker.io/fabiovelloso/tomcat7alpine:latest"
 oc import-image --confirm=true --insecure=true docker.io/fabiovelloso/tomcat7alpine:latest --namespace openshift
 echo ">>> Importing Image: docker.io/wnameless/oracle-xe-11g:latest"
 oc import-image --confirm=true --insecure=true docker.io/wnameless/oracle-xe-11g:latest --namespace openshift
+# Let user: demo into scc/anyuid
+echo ">>> Adding user demo into scc/anyuid"
+oadm policy add-scc-to-user anyuid demo
+# IMPORTANT: Add as many serviceaccounts into scc/privileged, based on project's name
+# Example: Let's suppose you wanted to create a project named "testbanco"
+#          oadm policy add-scc-to-user privileged system:serviceaccount:testbanco:velloso
